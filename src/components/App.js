4f93316c6f5b36e7f3785c280301365ac95831fa
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-18 13:58:52 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React  from 'react'
+import { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
-  
+  const [text , setText] = useState(true);
+  const handleClick = () => {
+    setText(!text);
+  }
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{text ? "Macro" : "Polo"}</h1>
+      <button id="marco-polo-toggler" onClick={handleClick}>
+        {text ? "Polo" : "Macro"}
+      </button>
     </div>
   )
 }
